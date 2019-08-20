81b345bbfc9eec49053b55decb6fb8783b17301e
Sergey <l.sergey.borzov.l@gmail.com>
2019-08-21 01:27:07 +0300

    blur horizontal bug fixed

--- src/framebuffers.cpp
+++ src/framebuffers.cpp
@@ -151,6 +151,8 @@ void PostProcessBuffer::BufferInit(int width, int height) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, brightMap, 0);
  
     glGenTextures(1, &horizontalGauss);
